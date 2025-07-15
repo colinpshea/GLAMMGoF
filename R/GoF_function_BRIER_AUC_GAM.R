68147dd3d6ab0c840d8ba56bd51c67e8168d29a9
Shea <colin.shea@myfwc.com>
2025-07-15 07:59:41 -0400

    Added predict.gam back to RE model for BRIER_AUC

--- R/GoF_function_BRIER_AUC_GAM.R
+++ R/GoF_function_BRIER_AUC_GAM.R
@@ -75,10 +75,10 @@ BRIER_AUC_GAM <- function(nReps = 100, testModel = NULL, testData = NULL, propTr
     if ("gam" %in% class(testModel)){
       if (length(testModel$smooth[lengths(lapply(testModel$smooth, function(x) x$random==TRUE))>0]) > 0){
         re_name <- testModel$smooth[lengths(lapply(testModel$smooth, function(x) x$random==TRUE))>0][[1]]$label
-        auc_train[j] <-  val.prob(p = predict(m_train, type="response", exclude = re_name, newdata = train), y = unname(unlist(eval(as.symbol(paste0("train")))[,all.vars(formula(testModel))[1]])), smooth = FALSE, pl = FALSE)["C (ROC)"]
-        brier_train[j] <- val.prob(p = predict(m_train, type="response", exclude = re_name, newdata = train), y = unname(unlist(eval(as.symbol(paste0("train")))[,all.vars(formula(testModel))[1]])), smooth = FALSE, pl = FALSE)["Brier"]
-        auc_test[j] <- val.prob(p = predict(m_train, type="response", exclude = re_name, newdata = test, newdata.guaranteed = TRUE), y = unname(unlist(eval(as.symbol(paste0("test")))[,all.vars(formula(testModel))[1]])), smooth = FALSE, pl = FALSE)["C (ROC)"]
-        brier_test[j] <- val.prob(p = predict(m_train, type="response", exclude = re_name, newdata = test, newdata.guaranteed = TRUE), y = unname(unlist(eval(as.symbol(paste0("test")))[,all.vars(formula(testModel))[1]])), smooth = FALSE, pl = FALSE)["Brier"]
+        auc_train[j] <-  val.prob(p = predict.gam(m_train, type="response", exclude = re_name, newdata = train), y = unname(unlist(eval(as.symbol(paste0("train")))[,all.vars(formula(testModel))[1]])), smooth = FALSE, pl = FALSE)["C (ROC)"]
+        brier_train[j] <- val.prob(p = predict.gam(m_train, type="response", exclude = re_name, newdata = train), y = unname(unlist(eval(as.symbol(paste0("train")))[,all.vars(formula(testModel))[1]])), smooth = FALSE, pl = FALSE)["Brier"]
+        auc_test[j] <- val.prob(p = predict.gam(m_train, type="response", exclude = re_name, newdata = test, newdata.guaranteed = TRUE), y = unname(unlist(eval(as.symbol(paste0("test")))[,all.vars(formula(testModel))[1]])), smooth = FALSE, pl = FALSE)["C (ROC)"]
+        brier_test[j] <- val.prob(p = predict.gam(m_train, type="response", exclude = re_name, newdata = test, newdata.guaranteed = TRUE), y = unname(unlist(eval(as.symbol(paste0("test")))[,all.vars(formula(testModel))[1]])), smooth = FALSE, pl = FALSE)["Brier"]
       }
     }
     if ("gam" %in% class(testModel)){
