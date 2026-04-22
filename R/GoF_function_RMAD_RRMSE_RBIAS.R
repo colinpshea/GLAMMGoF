0956277bb5ac80ecd7e286ad3cff58d1cce224f0
Shea <colin.shea@myfwc.com>
2026-04-22 10:09:10 -0400

    Fixed warning

--- R/GoF_function_RMAD_RRMSE_RBIAS.R
+++ R/GoF_function_RMAD_RRMSE_RBIAS.R
@@ -197,10 +197,9 @@ RRMSE_RMAD_RBIAS <- function(nReps = 100, testModel = NULL, testData = NULL,
   # ---- count up, report, and omit results with NA
   n_na <- sum(is.na(results_df$value) | is.infinite(results_df$value))
   if (n_na > 0)
-    message(n_na, " NA of Inf values removed from ", nrow(results_df),
-            " total bootstrap observations. ",
-            "This may indicate model instability or sparse data.")
-  results_df <- results_df[!is.na(results_df$value), ]
+    cat(n_na, " NA or Inf values removed from ", nrow(results_df),
+        " total bootstrap observations. ", "This may indicate model instability or sparse data.")
+  results_df <- results_df[!is.na(results_df$value) & !is.infinite(results_df$value), ]
 
   results_summary <- results_df %>%
     group_by(Group, Metric) %>%
