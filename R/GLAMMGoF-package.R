9b0e3da59df4e99d17c81876789cd4206b768c7d
Shea <colin.shea@myfwc.com>
2026-05-14 06:57:08 -0400

    Fixed some warnings

--- R/GLAMMGoF-package.R
+++ R/GLAMMGoF-package.R
@@ -1,4 +1,4 @@
 #' @importFrom stats family formula glm lm median predict quantile
 #' @importFrom ggplot2 coord_cartesian
 #' @importFrom mgcViz getViz
-utils::globalVariables(c("simRep", "metric", "Group", "Metric", "value", "mn"))
+utils::globalVariables(c("simRep", "metric", "Group", "Metric", "value", "mn", "null_value"))
